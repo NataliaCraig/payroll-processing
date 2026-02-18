1f81f2a0bcde1cadfc06bc7bd6af04c9aedcbf92
Natalia <nataliacraig@usf.edu>
2026-02-18 16:06:08 -0500

    added employee array

--- cc_5.js
+++ cc_5.js
@@ -1,1 +1,23 @@
-
+// Employee array 
+const employees = [
+    {
+    name: "Rylee", 
+    hourlyRate: 20,
+    hoursWorked: 38
+    },
+  {
+    name: "Jackson",
+    hourlyRate: 25,
+    hoursWorked: 45
+  },
+{
+    name: "Kara",
+    hourlyRate: 18, 
+    hoursWorked: 42
+},
+{
+    name: "Elly",
+    hourlyRate: 30,
+    hoursWorked: 40
+  }
+];
